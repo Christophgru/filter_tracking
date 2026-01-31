43cd9aac99b06f119d69837190820a8f087409d4
Christophgru <christoph@familie-gruender.de>
2026-01-31 17:41:08 +0100

    associateMeasurementsInit.m implemented

--- aufg_9/associateMeasurementsInit.m
+++ aufg_9/associateMeasurementsInit.m
@@ -1,8 +1,25 @@
 function A=associateMeasurementsInit(Z1,Z2)
-% Z1(i).z measurement of one object first time step
-% Z2(i).z measurement of one object second time step
+    % Z1(i).z measurement of one object first time step
+    % Z2(i).z measurement of one object second time step
+    
+    
+    A = zeros(numel(Z1));
+    % TODO: implement
+    for i =1:size(Z1)
+        closest_neighbor_of_i=0;
+        clostest_neighbor_dist=inf;
+    %calculate the closest neighbour j and then set association Matrix A (i,j)
+        for j=1:size(Z2)
+            dist=calc_distance(Z1(i).z,Z2(j).z);
+            if (dist<clostest_neighbor_dist)
+                closest_neighbor_of_i=j;
+                clostest_neighbor_dist=dist;
+            end
+        end
+        A(i,closest_neighbor_of_i)=1;
+    end
+end
 
-A = zeros(numel(Z1));
-% TODO: implement
-
-
+function dist=calc_distance(z1,z2)
+    dist=(z1.x - z2.x)^2 + (z1.y - z2.y)^2;
+end
